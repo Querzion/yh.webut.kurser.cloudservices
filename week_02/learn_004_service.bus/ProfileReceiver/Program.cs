42ca8bbb10cf36641f5513064771cfa72cd8feff
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-05-17 20:09:32 +0200

    Service bus test works, but my head does not do what it usually does which is to create 200 different solutions with it.

---
+++ week_02/learn_004_service.bus/ProfileReceiver/Program.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Messaging.ServiceBus;
+
+#region TOPIC
+
+    var connectionString = "Endpoint=sb://ventixe-assignment-slisk-sbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=8IinLH+B+hPhc2cQk4SUE8EaxJ6Q+zlBR+ASbNy/JIQ=";
+    var topicName = "profile-service";
+
+    var client = new ServiceBusClient(connectionString);
+    var processor = client.CreateProcessor(topicName, new ServiceBusProcessorOptions());
+
+    processor.ProcessMessageAsync += async args =>
+    {
+        var body = args.Message.Body.ToString();
+        Console.WriteLine("Received: " + body);
+        await args.CompleteMessageAsync(args.Message);
+    };
+
+    processor.ProcessErrorAsync += args =>
+    {
+        Console.WriteLine($"Error: {args.Exception}");
+        return Task.CompletedTask;
+    };
+
+    await processor.StartProcessingAsync();
+    Console.ReadKey();
+
+#endregion
---
+++ week_02/learn_004_service.bus/Publisher/Program.cs
@@ -0,0 +1,47 @@
+﻿/* PUBLISHER */
+using Azure.Messaging.ServiceBus;
+
+#region QUEUE
+
+    // var connectionString = "Endpoint=sb://ventixe-assignment-slisk-sbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=8IinLH+B+hPhc2cQk4SUE8EaxJ6Q+zlBR+ASbNy/JIQ=";
+    // var queueName = "email-service";
+    //
+    // var client = new ServiceBusClient(connectionString);
+    // var sender = client.CreateSender(queueName);
+    //
+    // Console.WriteLine("Press any key to send message.");
+    // Console.ReadKey();
+    //
+    // var message = new ServiceBusMessage("slisk.lindqvist@querzion.com");
+    // await sender.SendMessageAsync(message);
+    //
+    // await sender.DisposeAsync();
+    // await client.DisposeAsync();
+    //
+    // Console.ReadKey();
+
+#endregion
+
+#region TOPIC
+
+    var connectionString = "Endpoint=sb://ventixe-assignment-slisk-sbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=8IinLH+B+hPhc2cQk4SUE8EaxJ6Q+zlBR+ASbNy/JIQ=";
+    var topicName = "account-created";
+
+    var client = new ServiceBusClient(connectionString);
+    var sender = client.CreateSender(topicName);
+
+    Console.WriteLine("Press any key to send message.");
+    Console.ReadKey();
+
+    var message = new ServiceBusMessage("slisk.lindqvist@querzion.com");
+    // Doesn't need to be here, it's an added property.
+    message.ApplicationProperties.Add("accountType", "admin");
+    
+    await sender.SendMessageAsync(message);
+
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+
+    Console.ReadKey();
+
+#endregion
